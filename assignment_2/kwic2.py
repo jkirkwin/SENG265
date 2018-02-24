aff2ac580709d40703a8f0cc77d94f905d65e255
Jamie Kirkwin <jkirkwin@gmail.com>
2018-02-23 19:37:39 -0700

    completed get_input()

--- assignment_2/kwic2.py
+++ assignment_2/kwic2.py
@@ -15,6 +15,28 @@ excluded = list()
 input_lines = list()
 output_lines = list()
 
+# for debugging
+# stackoverflow.com/questions/12214801/print-a-string-as-hex-bytes
+def print_hex(l):
+    for s in l:
+        print(":".join("{:02x}".format(ord(c)) for c in s))
+
+# for debugging
+def print_lines(l):
+    for s in l:
+        print(s)
+
+
+# for debugging
+# should output the exact input file
+def print_input():
+    print(2)
+    print('::')
+    print_lines(excluded)
+    print('::')
+    print_lines(input_lines)
+
+
 # Prints the fully indexed list of words with the formatting specified in 
 # a2_writeup.pdf
 def print_output():
@@ -46,20 +68,32 @@ def get_input():
             continue
         
         elif flag:
-            # getting input lines
-            x = 1
+            # non-empty input line
+            input_lines.append(line)
+
+            # check each word against excluded words and previously added index words
+            words = line.split()
+            for w in words:
+                if w not in (excluded + index_words):
+                    index_words.append(w)  
+
         else:
             # still getting excluded words
             excluded.append(line)
+
+        index_words.sort()
 
     return
 
 
 def main():
     get_input()
-    print(excluded)
+    
+    #print_input()
+    print()
+    print_lines(index_words)
+    
     index()
-
     print_output()
 
 if __name__ == '__main__':
