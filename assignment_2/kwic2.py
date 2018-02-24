7ae9a06f7b3a958fd64e4bd59d542286ebabaeb8
Jamie Kirkwin <jkirkwin@gmail.com>
2018-02-23 17:55:36 -0700

    Created kwic2.py

---
+++ assignment_2/kwic2.py
@@ -0,0 +1,14 @@
+# Add shebang path
+
+# Jamie Kirkwin
+# Seng 265 Assignment 2
+# V00875987
+# kwic2.c
+# Feb 23, 2018
+# Intro to Python, indexing a text file by keyword with basic formatting
+
+def main():
+    
+
+if __name__ == '__main__':
+    main()
