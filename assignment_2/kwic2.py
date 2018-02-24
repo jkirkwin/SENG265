0bd65f07b970d1d7b0cb84b20b0d355583d7f8c5
Jamie Kirkwin <jkirkwin@gmail.com>
2018-02-23 18:20:54 -0700

    Skeleton of major functions completed

--- assignment_2/kwic2.py
+++ assignment_2/kwic2.py
@@ -7,8 +7,34 @@
 # Feb 23, 2018
 # Intro to Python, indexing a text file by keyword with basic formatting
 
+index_words = list()
+excluded = list()
+input_lines = list()
+output_lines = list()
+
+# Prints the fully indexed list of words with the formatting specified in 
+# a2_writeup.pdf
+def print_output():
+    return
+
+
+# Fills the output_lines list with the indexed compliment of input_lines
+# according to the content of the lists of excluded and index words 
+def index():
+    return
+
+
+# Records input line-by-line in a input_lines
+# Fills and sorts the lists of excluded and index words 
+def get_input():
+    return
+
+
 def main():
-    
+    get_input()
+    index()
+    print_output()
+
 
 if __name__ == '__main__':
     main()
