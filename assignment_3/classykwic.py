07d378c3d4a89aea8d88add1c9e1de265544faee
Jamie Kirkwin <jkirkwin@gmail.com>
2018-03-13 15:51:28 -0600

    Modified gitignore, added basic functionality to classykwic

--- assignment_3/classykwic.py
+++ assignment_3/classykwic.py
@@ -4,9 +4,11 @@
 Jamie Kirkwin
 Seng 265 Assignment 3
 classykwic.py
-March 12, 2018
+March 13, 2018
 
 Refactor kwic2 using regular expressions and a Kwic class
+
+The program using this class is kwic3.py
 '''
 
 import sys
@@ -14,53 +16,29 @@ import classykwic
 
 class Kwic:
 
+    # formatting pad values (class variables)
+    start = 10
+    centre = 30
+    end = 60
+
+    def get_index_words(self):
+        '''Creates a list of all non-excluded words'''
+        
+        index_words = []
+        for line in self.raw_lines:
+            for word in line.split():
+                if word not in (self.excluded + index_words):
+                    index_words.append(word) 
+
+        return index_words
+
+
     def __init__(self, excluded, lines):
-        self.excluded = []
-        self.index_lines = []
+        self.excluded = excluded
+        self.raw_lines = lines
+        self.index_words = self.get_index_words()
 
 
     def output(self):
+        '''returns the fully indexed list of lines'''
         return []
-
-
-def main():
-''' code provided by Dr Zastre with assignment '''
-
-    count = 0
-    mode = 0   # 0 = version; 1 = exclusion; 2 = actual lines
-    exclusion = 0
-
-    excluded = []
-    input_lines = []
-
-    for line in sys.stdin:
-        line = line.rstrip()
-        if (mode == 0 and line != "::"):
-            kwic_version = int(line)
-            if (kwic_version != 2):
-                print("File has version ", kwic_version,
-                    ", expected 2")
-                sys.exit(1)
-
-            continue
-        elif (line == "::" and mode == 0):
-            mode = 1
-            continue
-        elif (line == "::" and mode == 1):
-            mode = 2
-            continue
-
-        if (mode == 1):
-            excluded.append(line.lower())
-        elif (mode == 2):
-            if (line != ""):
-                input_lines.append(line)
-
-    k = classykwic.Kwic(excluded, input_lines)
-
-    for l in k.output():
-        print(l)
-
-
-if __name__ == "__main__":
-    main()
