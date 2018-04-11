610e9555865aaa5ffb054aee49517a132e7391f9
Jamie Kirkwin <jkirkwin@uvic.ca>
2018-04-10 22:44:06 -0700

    added mindblowing hello world functionality

--- Final Exam Review/git_gymnastics/somefile.c
+++ Final Exam Review/git_gymnastics/somefile.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
+void hellontimes(int n)
+{
+    int i;
+    for(int i = 0; i < n; i++)
+    {
+        printf("Hello ");
+        if(i == 0) printf("World!\n");
+        else printf("Again!\n");
+    }
+}
+
+
 int main()
 {
-    printf("Hello World!\n");
+    printf("Hello!\n");
+    hellontimes(2);
+
     return 0;
 }
